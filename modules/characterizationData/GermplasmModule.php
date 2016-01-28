384914d09bd727a444695df2d50f419932a163ee
Nikki Carumba <n.carumba@irri.org>
2016-01-28 11:31:36 +0800

    Add germplasm module.

---
+++ modules/characterizationData/GermplasmModule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\characterizationData;
+
+class GermplasmModule extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\characterizationData\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
