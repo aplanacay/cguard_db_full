d75bdf29fdd0d853b2cd0535d82af03c4f343df5
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2016-03-23 10:34:46 +0800

    Change database settings.

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=phlgrims;port=5432;',
-    'username' => 'postgres',
-    'password' => 'postgres',
+    'dsn' => 'pgsql:host=phlgrims.c6woeyfvbwty.ap-southeast-1.rds.amazonaws.com;dbname=phlgrims;port=5432;',
+    'username' => 'phlgrims',
+    'password' => 'phlgrims',
     'charset' => 'utf8',
 ];
