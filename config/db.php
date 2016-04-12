32f6c1bcbb43769ec472d27106a29ed1e8f7f4a9
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2016-04-12 19:20:34 +0800

    Change access to accession.

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=phlgrims.c6woeyfvbwty.ap-southeast-1.rds.amazonaws.com;dbname=phlgrims;port=5432;',
-    'username' => 'phlgrims',
-    'password' => 'phlgrims',
+    //'dsn' => 'pgsql:host=phlgrims.c6woeyfvbwty.ap-southeast-1.rds.amazonaws.com;dbname=phlgrims;port=5432;',
+    'dsn' => 'pgsql:host=localhost;dbname=phlgrims;port=5432;',
+    'username' => 'postgres',
+    'password' => 'postgres',
     'charset' => 'utf8',
 ];
