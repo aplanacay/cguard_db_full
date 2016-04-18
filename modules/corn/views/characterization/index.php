95e6bb80ecc8b61f165dc45c9a26abd0735b643c
Nikki Carumba <n.carumba@irri.org>
2016-04-19 04:45:56 +0800

    Set blank for null values instead of (not set).

--- modules/corn/views/characterization/index.php
+++ modules/corn/views/characterization/index.php
@@ -194,7 +194,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'showPersonalize' => true,
                 'storage' => 'cookie',
                 'gridOptions' => [
-
+                    'formatter' => ['class' => 'yii\i18n\Formatter', 'nullDisplay' => ''],
                     'bordered' => false,
                     'condensed' => true,
                     //'export' => false,
