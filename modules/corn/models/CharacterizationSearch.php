753f20c533f94e5ae16293c2315a16d19e08192e
Nikki Carumba <n.carumba@irri.org>
2016-05-06 11:48:51 +0800

    Add create characterization feature.

--- modules/corn/models/CharacterizationSearch.php
+++ modules/corn/models/CharacterizationSearch.php
@@ -11,27 +11,7 @@ use kartik\builder\Form;
 /**
  * CharacterizationSearch represents the model behind the search form about `app\models\CharacterizationBase`.
  */
-class CharacterizationSearch extends CharacterizationBase {
-
-    /**
-     * @inheritdoc
-     */
-    public function rules() {
-        return [
-            [['id', 'germplasm_id', 'days_to_emergence', 'days_to_tasseling', 'days_to_slking', 'days_to_harvest', 'days_to_ear_leaf_inflorescence', 'stalk_lodging', 'grain_shedding', 'creator_id', 'modifier_id'], 'integer'],
-            [['tillering_index', 'total_number_of_leaves_per_plant', 'leaf_length', 'leaf_width', 'venation_index', 'unshelled_weight', 'shelled_weight', 'kernel_weight', 'shellpc'], 'number'],
-            [['stem_color', 'sheath_pubescence', 'leaf_orientation', 'presence_of_leaf_ligule', 'tassel_type', 'silk_color', 'tassel_color', 'plant_height', 'ear_height', 'foliage', 'number_of_leaves_above_upper_ear', 'number_of_leaves_below_upper_ear', 'number_of_internodes_below_uppermost_ear', 'number_of_internodes_on_the_whole_stem', 'stem_diameter_at_the_base', 'stem_diameter_below_uppermost_ear', 'tassel_length', 'tassel_peduncle_length', 'tassel_branching_space', 'number_of_primary_branches_on_tassel', 'number_of_secondary_branches_on_tassel', 'number_of_tertiary_branches_on_tassel', 'stay_green', 'husk_cover', 'husk_fitting', 'husk_tip_shape', 'ear_shape', 'ear_tip_shape', 'ear_orientation', 'ear_length', 'ear_diameter', 'cob_diameter', 'rachs_diameter', 'peduncle_length', 'number_of_bracts', 'kernel_row_arrangement', 'number_of_kernel_rows', 'number_of_kernels__per_row', 'cob_color', 'kernel_type', 'kernel_color', 'kernel_length', 'kernel_width', 'kernel_thickness', 'shape_of_upper_kernel_surface', 'pericarp_color', 'aleurone_color', 'endosperm_color', 'creation_timestamp', 'modification_timestamp', 'remarks', 'Notes'], 'safe'],
-            [['is_void'], 'boolean'],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function scenarios() {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+class CharacterizationSearch extends Characterization {
 
     /**
      * Creates data provider instance with search query applied
@@ -41,7 +21,7 @@ class CharacterizationSearch extends CharacterizationBase {
      * @return ActiveDataProvider
      */
     public function search($params) {
-        $query = CharacterizationBase::find();
+        $query = Characterization::find();
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
